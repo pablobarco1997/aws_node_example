fc868d91d5ecfdabb0eb5cfbe9f589ef5d5fee75
barcoMarquez <pablob.marquez@hotmail.com>
2022-01-07 23:14:43 -0500

    otro commit

--- modules/socket.io_/io.js
+++ modules/socket.io_/io.js
@@ -11,7 +11,7 @@ class ClassIOsocket{
         
         this.IO = socket(server, {
             cors: {
-                origin: "http://localhost",
+                origin: 'http://ec2-18-117-195-56.us-east-2.compute.amazonaws.com/',
                 methods: ["GET", "POST"]
               }
         }); 
