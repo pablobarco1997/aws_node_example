d0ab6b806df3d5af195698919a411cf64315566a
barcoMarquez <pablob.marquez@hotmail.com>
2022-01-07 22:42:09 -0500

    otro commit

--- public/web.php
+++ public/web.php
@@ -14,7 +14,8 @@ define('HTTP_HOST', 'http://'.$_SERVER['SERVER_NAME']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="<?= 'http://localhost/aws_node_example/public/js/jquery-qrcode-0.18.0.min.js' ?> "></script>
+
+    <script src="<?= HTTP_HOST.'/aws_node_example/public/js/jquery-qrcode-0.18.0.min.js' ?> "></script>
 
         <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
